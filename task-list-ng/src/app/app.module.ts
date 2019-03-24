95a128d01d31038af25a39c24ac7552a8b6ed1fc
yuya-yuliya <yuya.yuliya@gmail.com>
2019-03-24 14:59:06 +0300

    Create task-edit component.

--- task-list-ng/src/app/app.module.ts
+++ task-list-ng/src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { TaskService } from './services/task.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { TaskService } from "./services/task.service";
+import { TaskEditComponent } from "./task-edit/task-edit.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
+  declarations: [AppComponent, MainPageComponent, TaskEditComponent],
   imports: [
     // Angular imports
     BrowserModule,
@@ -21,7 +19,7 @@ import { TaskService } from './services/task.service';
     NgbModule,
     HttpClientModule,
     // Application imports
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [
     // Angular providers
@@ -31,4 +29,4 @@ import { TaskService } from './services/task.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- task-list-ng/src/app/main-page/main-page.component.ts
+++ task-list-ng/src/app/main-page/main-page.component.ts
@@ -10,25 +10,41 @@ import { saveAs } from "file-saver";
 })
 export class MainPageComponent implements OnInit {
   tasks: Task[];
-  visibleTasks: Task[];
   filter: boolean;
   editPanel: boolean;
-  existed = false;
-  task: Task;
-  attachedFile;
+  selectedIndex: number;
 
   constructor(private taskService: TaskService) {}
+
+  get visibleTasks(): Task[] {
+    if (this.filter !== undefined) {
+      return this.tasks.filter(task => task.completed == this.filter);
+    } else {
+      return this.tasks;
+    }
+  }
 
   ngOnInit() {
     this.getTasks();
     this.editPanel = false;
   }
 
+  editHide(value: boolean) {
+    this.editPanel = value;
+  }
+
   getTasks() {
-    this.taskService.getTasks().subscribe(t => {
-      this.tasks = t;
-      this.visibleTasks = this.tasks;
-    });
+    this.taskService.getTasks().subscribe(
+      t => {
+        this.tasks = t;
+        this.filter = undefined;
+      },
+      error => {
+        this.tasks = [
+          new Task("1", "my", false, new Date(), undefined, undefined)
+        ];
+      }
+    );
   }
 
   onFilterChange(filterValue) {
@@ -37,19 +53,6 @@ export class MainPageComponent implements OnInit {
     } else {
       this.filter = undefined;
     }
-    this.visibleTasks = this.filterChange(this.filter);
-  }
-
-  onDelete(taskId: string) {
-    this.taskService.deleteTask(taskId).subscribe(o => {
-      this.filter = undefined;
-      const index: number = this.tasks.findIndex(value => value._id == taskId);
-      if (index != -1) {
-        this.tasks.splice(index, 1);
-        this.visibleTasks = this.tasks;
-      }
-      this.onEditClose();
-    });
   }
 
   onStatusChange(taskId: string, completed: boolean) {
@@ -57,56 +60,18 @@ export class MainPageComponent implements OnInit {
       const index: number = this.tasks.findIndex(value => value._id == taskId);
       if (index != -1) {
         this.tasks[index].completed = completed;
-        this.visibleTasks = this.filterChange(this.filter);
       }
     });
   }
 
   onEdit(taskId: string) {
-    this.task = this.tasks.find(value => value._id === taskId);
-    this.existed = true;
+    this.selectedIndex = this.tasks.findIndex(value => value._id === taskId);
     this.editPanel = true;
-  }
-
-  filterChange(filterValue: boolean): Task[] {
-    if (filterValue != undefined) {
-      return this.tasks.filter(value => value.completed == filterValue);
-    } else {
-      return this.tasks;
-    }
   }
 
   onTaskAdd() {
     this.editPanel = true;
-    this.existed = false;
-    this.task = new Task("", "", false, new Date(), "", "");
-  }
-
-  onEditClose() {
-    this.editPanel = false;
-    this.existed = false;
-    this.attachedFile = undefined;
-  }
-
-  onSubmit() {
-    if (!this.existed) {
-      this.taskService.addTask(this.task, this.attachedFile).subscribe(task => {
-        this.tasks.push(task);
-        this.onEditClose();
-      });
-    } else {
-      this.taskService
-        .updateTask(this.task, this.attachedFile)
-        .subscribe(task => {
-          let index = this.tasks.findIndex(value => value._id === task._id);
-          this.tasks[index] = task;
-          this.onEditClose();
-        });
-    }
-  }
-
-  onFileChange(event) {
-    this.attachedFile = event.target.files.item(0);
+    this.selectedIndex = -1;
   }
 
   onDownload(taskId: String) {
